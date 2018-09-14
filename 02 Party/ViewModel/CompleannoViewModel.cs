317ead149ed5461bead1d54f60505ce316d218a0
Giulio Bosco <giuliobva@gmail.com>
2018-09-14 19:10:49 +0200

    add 02Party project - finished

---
+++ 02 Party/ViewModel/CompleannoViewModel.cs
@@ -0,0 +1,87 @@
+﻿using _02_Party.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Party.ViewModel {
+    class CompleannoViewModel : INotifyPropertyChanged {
+
+        #region =================== costanti ===================
+        #endregion
+
+        #region =================== membri statici =============
+        #endregion
+
+        #region =================== membri & proprieta =========
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private Compleanno model;
+
+
+        public int Persone {
+            get { return model.Persone; }
+            set {
+                if (model.Persone == (int)value) {
+                    return;
+                }
+                model.Persone = value;
+                OnPropertyChanged("Persone");
+                OnPropertyChanged("Totale");
+            }
+        }
+
+        public string Testo {
+            get { return model.Testo; }
+            set {
+                if (model.Testo == (string)value) {
+                    return;
+                }
+                model.Testo = value;
+                OnPropertyChanged("Testo");
+                OnPropertyChanged("Totale");
+            }
+        }
+
+        public bool DecorazioneProfessionale {
+            get { return model.DecorazioneProfessionale; }
+            set {
+                if (model.DecorazioneProfessionale == (bool)value) {
+                    return;
+                }
+                model.DecorazioneProfessionale = value;
+                OnPropertyChanged("DecorazioneProfessionale");
+                OnPropertyChanged("Totale");
+            }
+        }
+
+        public double Totale {
+            get { return model.CalcolaTotale(); }
+        }
+
+        #endregion
+
+        #region =================== costruttori ================
+
+        public CompleannoViewModel() {
+            model = new Compleanno();
+        }
+
+        #endregion
+
+        #region =================== metodi aiuto ===============
+        #endregion
+
+        #region =================== metodi generali ============
+
+        protected void OnPropertyChanged(string propertyName) {
+			PropertyChangedEventHandler handler = PropertyChanged;
+			if (handler != null)
+				handler(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+        #endregion
+    }
+}
---
+++ 02 Party/ViewModel/EventoViewModel.cs
@@ -0,0 +1,84 @@
+﻿using _02_Party.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Party.ViewModel {
+    public class EventoViewModel : INotifyPropertyChanged {
+
+        #region =================== costanti ===================
+        #endregion
+
+        #region =================== membri statici =============
+        #endregion
+
+        #region =================== membri & proprieta =========
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private Evento model;
+
+        public int Persone {
+            get { return model.Persone; }
+            set {
+                if (model.Persone == (int)value) {
+                    return;
+                }
+                model.Persone = value;
+                OnPropertyChanged("Persone");
+                OnPropertyChanged("Totale");
+            }
+        }
+
+        public bool DecorazioneProfessionale {
+            get { return model.DecorazioneProfessionale; }
+            set {
+                if (model.DecorazioneProfessionale == (bool)value) {
+                    return;
+                }
+                model.DecorazioneProfessionale = value;
+                OnPropertyChanged("DecorazioneProfessionale");
+                OnPropertyChanged("Totale");
+            }
+        }
+
+        public bool Alcool {
+            get { return model.Alcool; }
+            set {
+                if (model.Alcool == (bool)value) {
+                    return;
+                }
+                model.Alcool = value;
+                OnPropertyChanged("Alcool");
+                OnPropertyChanged("Totale");
+            }
+        }
+
+        public double Totale {
+            get { return model.CostoTotale(); }
+        }
+        #endregion
+
+        #region =================== costruttori ================
+        public EventoViewModel() {
+            model = new Evento();
+        }
+
+        #endregion
+
+        #region =================== metodi aiuto ===============
+        #endregion
+
+        #region =================== metodi generali ============
+
+        protected void OnPropertyChanged(string propertyName) {
+			PropertyChangedEventHandler handler = PropertyChanged;
+			if (handler != null)
+				handler(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+        #endregion
+    }
+}
