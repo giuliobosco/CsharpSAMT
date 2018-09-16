27967c141c1ed37859d4cd30baa1df91c77e18cd
Giulio Bosco <giuliobva@gmail.com>
2018-09-16 17:15:52 +0200

    add 03 Tariffa Project - Tariffa Calcule Class

---
+++ 03 Tariffa/ViewModel/TariffaViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _03_Tariffa.Model;
+
+namespace _03_Tariffa.ViewModel {
+    public class TariffaViewModel : INotifyPropertyChanged {
+        #region =================== costanti ===================
+        #endregion
+
+        #region =================== membri statici =============
+        #endregion
+
+        #region =================== membri & proprieta =========
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private Tariffa model;
+        #endregion
+
+        #region =================== costruttori ================
+        public TariffaViewModel() {
+            model = new Tariffa();
+        }
+        #endregion
+
+        #region =================== metodi aiuto ===============
+        #endregion
+
+        #region =================== metodi generali ============
+
+        protected void OnPropertyChanged(string propertyName) {
+			PropertyChangedEventHandler handler = PropertyChanged;
+			if (handler != null)
+				handler(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+        #endregion
+    }
+}
