69403aab20dc28e2b05ab4d28ff2c98575bfef81
Giulio Bosco <giuliobva@gmail.com>
2018-09-26 21:49:28 +0200

    add 04 Weather.cs - empty class

---
+++ 04 Weather/Model/Weather.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Weather.Model {
+    public class Weather {
+        #region =================== costanti ===================
+        #endregion
+
+        #region =================== membri statici =============
+        #endregion
+
+        #region =================== membri & propriet‡ =========
+        #endregion
+
+        #region =================== costruttori ================
+        #endregion
+
+        #region =================== metodi aiuto ===============
+        #endregion
+
+        #region =================== metodi generali ============
+        #endregion
+    }
+}
