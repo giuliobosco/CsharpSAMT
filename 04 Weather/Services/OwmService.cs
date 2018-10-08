d2c028e12a4e44b5c3d0b8427982aca89007afc3
Giulio Bosco <giuliobva@gmail.com>
2018-10-08 18:58:11 +0200

    fix 04 OwmService - set all parameters

--- 04 Weather/Services/OwmService.cs
+++ 04 Weather/Services/OwmService.cs
@@ -7,6 +7,7 @@ using _04_Weather.Model;
 using System.Net.Http;
 using System.Xml.Linq;
 using System.IO;
+using System.Windows;
 
 namespace _04_Weather.Services {
     public class OwmService : IOwmService {
@@ -35,6 +36,56 @@ namespace _04_Weather.Services {
             string query = "weather?q=" + city + "&appid=" + ks.GetKey();
             return BASEURL + query + "&mode=xml&units=metric";
         }
+
+        private OpenWeatherMap getOwmFromXml(string stringElement) {
+            var e = XElement.Load(new StringReader(stringElement));
+
+            OpenWeatherMap owm = new OpenWeatherMap();
+
+            // Load city elements
+            owm.City.Id = int.Parse(e.Element("city").Attribute("id").Value);
+            owm.City.Name = e.Element("city").Attribute("name").Value;
+            owm.City.Country = e.Element("city").Element("country").Value;
+            owm.City.Coordinates = new Point(
+                double.Parse(e.Element("city").Element("coord").Attribute("lon").Value),
+                double.Parse(e.Element("city").Element("coord").Attribute("lat").Value));
+            owm.City.Sunrise = DateTime.Parse(e.Element("city").Element("sun").Attribute("rise").Value);
+            owm.City.Sunset = DateTime.Parse(e.Element("city").Element("sun").Attribute("set").Value);
+
+            // Load Temperature
+            owm.Temperature.MinValue = double.Parse(e.Element("temperature").Attribute("min").Value);
+            owm.Temperature.MaxValue = double.Parse(e.Element("temperature").Attribute("max").Value);
+            owm.Temperature.Value = double.Parse(e.Element("temperature").Attribute("value").Value);
+
+            // Humidity
+            owm.Humidity = double.Parse(e.Element("humidity").Attribute("value").Value);
+
+            // pressure
+            owm.Pressure = double.Parse(e.Element("pressure").Attribute("value").Value);
+
+            // wind
+            owm.Wind.Speed = double.Parse(e.Element("wind").Element("speed").Attribute("value").Value);
+            owm.Wind.Name = e.Element("wind").Element("speed").Attribute("name").Value;
+            owm.Wind.WindDirection = double.Parse(e.Element("wind").Element("direction").Attribute("value").Value);
+            owm.Wind.WindCode = e.Element("wind").Element("direction").Attribute("code").Value;
+            owm.Wind.WindName = e.Element("wind").Element("direction").Attribute("name").Value;
+
+            // clouds
+            owm.Clouds.Value = double.Parse(e.Element("clouds").Attribute("value").Value);
+            owm.Clouds.Name = e.Element("clouds").Attribute("name").Value;
+
+            owm.Visibility = double.Parse(e.Element("visibility").Attribute("value").Value);
+            owm.Precipitation = double.Parse(e.Element("precipitation").Attribute("value").Value);
+
+            // Weather
+            owm.Weather.Id = int.Parse(e.Element("weather").Attribute("number").Value);
+            owm.Weather.Icon = e.Element("weather").Attribute("icon").Value;
+            owm.Weather.Description = e.Element("Weather").Attribute("value").Value;
+
+            // last update
+            owm.LastUpdate = DateTime.Parse(e.Element("lastupdate").Attribute("value").Value);
+            return owm;
+        }
         #endregion
 
         #region =================== metodi generali ============
@@ -44,22 +95,7 @@ namespace _04_Weather.Services {
                     string path = ConstructUrl(city);
                     var result = client.GetStringAsync(path).Result;
 
-                    var x = XElement.Load(new StringReader(result));
-
-                    OpenWeatherMap owm = new OpenWeatherMap();
-
-                    owm.City.Name = x.Element("city").Attribute("name").Value;
-
-                    owm.Temperature.MinValue = double.Parse(x.Element("temperature").Attribute("min").Value);
-                    owm.Temperature.MaxValue = double.Parse(x.Element("temperature").Attribute("max").Value);
-                    owm.Temperature.Value = double.Parse(x.Element("temperature").Attribute("value").Value);
-
-                    // ws.WindDirection = x.Element("wind").Element("direction").Attribute("name").Value;
-
-                    owm.Weather.Id = int.Parse(x.Element("weather").Attribute("number").Value);
-                    owm.Weather.Icon = x.Element("weather").Attribute("icon").Value;
-                    owm.Weather.Description = x.Element("weather").Attribute("value").Value;
-
+                    OpenWeatherMap owm = this.getOwmFromXml(result);
                     return owm;
                 } catch (Exception) {
 
