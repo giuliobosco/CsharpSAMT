df86d703948868f31b33379aa87fad72eea596a0
Giulio Bosco <giuliobva@gmail.com>
2018-10-08 15:50:48 +0200

    add 05 Weather-Weather

---
+++ 04 Weather/Model/Weather/Weather.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Weather.Model.Weather {
+    public class Weather {
+        #region =================== costants ===================
+        #endregion
+
+        #region =================== static Members =============
+        #endregion
+
+        #region =================== properties & members =======
+        public int Id { get; set; }
+
+        private int description;
+
+        public int Description {
+            get { return description; }
+            set { description = value; }
+        }
+
+        private string icon;
+
+        public string Icon {
+            get { return icon; }
+            set { icon = value; }
+        }
+
+        #endregion
+
+        #region =================== constructors ===============
+        #endregion
+
+        #region =================== help methods ===============
+        #endregion
+
+        #region =================== general methods ============
+        #endregion
+    }
+}
