8663c611928f8853a7afa1aa3794f9703be4fc0b
Giulio Bosco <giuliobva@gmail.com>
2018-10-08 16:00:39 +0200

    add 04 OWM KeyService - Static Key

---
+++ 04 Weather/Services/IKeyService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Weather.Services {
+    public interface IKeyService {
+        string GetKey();
+    }
+}
