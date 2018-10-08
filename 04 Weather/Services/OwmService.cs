66bc2a117f7a54ec96de5217bbd68dc23563b89b
Giulio Bosco <giuliobva@gmail.com>
2018-10-08 19:18:08 +0200

    fix 04 OwmService - Split getOwmFromUML

--- 04 Weather/Services/OwmService.cs
+++ 04 Weather/Services/OwmService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using _04_Weather.Model;
+using _04_Weather.Model.Weather;
 using System.Net.Http;
 using System.Xml.Linq;
 using System.IO;
@@ -37,53 +38,134 @@ namespace _04_Weather.Services {
             return BASEURL + query + "&mode=xml&units=metric";
         }
 
+        /*
+         * Get the city object form the xml city element.
+         * 
+         * @param e XML city element.
+         * @return City object with the information on the XML element.
+         */
+        public City getCity(XElement e) {
+            // create city
+            City city = new City();
+
+            // Load elements
+            city.Id = int.Parse(e.Attribute("id").Value);
+            city.Name = e.Attribute("name").Value;
+            city.Country = e.Element("country").Value;
+            city.Coordinates = new Point(
+                double.Parse(e.Element("coord").Attribute("lon").Value),
+                double.Parse(e.Element("coord").Attribute("lat").Value));
+            city.Sunrise = DateTime.Parse(e.Element("sun").Attribute("rise").Value);
+            city.Sunset = DateTime.Parse(e.Element("sun").Attribute("set").Value);
+
+            return city;
+        }
+
+        /**
+         * Get the Temperature object form the XML temperature element.
+         * 
+         * @param e XML temperature element.
+         * @return Temperature object with the information on the XML element.
+         */
+        private Temperature getTemperature(XElement e) {
+            // create temperature object.
+            Temperature t = new Temperature();
+
+            // Load data
+            t.MinValue = double.Parse(e.Attribute("min").Value);
+            t.MaxValue = double.Parse(e.Attribute("max").Value);
+            t.Value = double.Parse(e.Attribute("value").Value);
+
+            return t;
+        }
+
+        /**
+         * Get the Wind object form the XML temperature element.
+         * 
+         * @param e XML wind element.
+         * @return Wind object with the information on the XML element.
+         */
+        private Wind getWind(XElement e) {
+            // create wind object.
+            Wind wind = new Wind();
+
+            // Load data
+            wind.Speed = double.Parse(e.Element("speed").Attribute("value").Value);
+            wind.Name = e.Element("speed").Attribute("name").Value;
+            wind.WindDirection = double.Parse(e.Element("direction").Attribute("value").Value);
+            wind.WindCode = e.Element("direction").Attribute("code").Value;
+            wind.WindName = e.Element("direction").Attribute("name").Value;
+
+            return wind;
+        }
+
+        /**
+         * Get the Weather object form the XML temperature element.
+         * 
+         * @param e XML weather element.
+         * @return Weather object with the information on the XML element.
+         */
+         private Weather getWeather(XElement e) {
+            // create weather element.
+            Weather w = new Weather();
+
+            // Load data
+            w.Id = int.Parse(e.Attribute("number").Value);
+            w.Icon = e.Attribute("icon").Value;
+            w.Description = e.Attribute("value").Value;
+
+            return w;
+        }
+
+        /**
+         * Get the clouds object form the XML temperature element.
+         * 
+         * @param e XML clouds element.
+         * @return clouds object with the information on the XML element.
+         */
+        private Clouds getClouds(XElement e) {
+            // create clouds element
+            Clouds c = new Clouds();
+
+            // load data
+            c.Value = double.Parse(e.Attribute("value").Value);
+            c.Name = e.Attribute("name").Value;
+
+            return c;
+        }
+
+        /**
+         * Get the OpenWeatherMap object form the XML.
+         * 
+         * @param stringElement String XML data.
+         * @return The OpenWeatherMap object.
+         */
         private OpenWeatherMap getOwmFromXml(string stringElement) {
             var e = XElement.Load(new StringReader(stringElement));
 
             OpenWeatherMap owm = new OpenWeatherMap();
 
             // Load city elements
-            owm.City.Id = int.Parse(e.Element("city").Attribute("id").Value);
-            owm.City.Name = e.Element("city").Attribute("name").Value;
-            owm.City.Country = e.Element("city").Element("country").Value;
-            owm.City.Coordinates = new Point(
-                double.Parse(e.Element("city").Element("coord").Attribute("lon").Value),
-                double.Parse(e.Element("city").Element("coord").Attribute("lat").Value));
-            owm.City.Sunrise = DateTime.Parse(e.Element("city").Element("sun").Attribute("rise").Value);
-            owm.City.Sunset = DateTime.Parse(e.Element("city").Element("sun").Attribute("set").Value);
-
+            owm.City = this.getCity(e.Element("city"));
             // Load Temperature
-            owm.Temperature.MinValue = double.Parse(e.Element("temperature").Attribute("min").Value);
-            owm.Temperature.MaxValue = double.Parse(e.Element("temperature").Attribute("max").Value);
-            owm.Temperature.Value = double.Parse(e.Element("temperature").Attribute("value").Value);
-
+            owm.Temperature = this.getTemperature(e.Element("temperature"));
             // Humidity
             owm.Humidity = double.Parse(e.Element("humidity").Attribute("value").Value);
-
             // pressure
             owm.Pressure = double.Parse(e.Element("pressure").Attribute("value").Value);
-
             // wind
-            owm.Wind.Speed = double.Parse(e.Element("wind").Element("speed").Attribute("value").Value);
-            owm.Wind.Name = e.Element("wind").Element("speed").Attribute("name").Value;
-            owm.Wind.WindDirection = double.Parse(e.Element("wind").Element("direction").Attribute("value").Value);
-            owm.Wind.WindCode = e.Element("wind").Element("direction").Attribute("code").Value;
-            owm.Wind.WindName = e.Element("wind").Element("direction").Attribute("name").Value;
-
+            owm.Wind = this.getWind(e.Element("wind"));
             // clouds
-            owm.Clouds.Value = double.Parse(e.Element("clouds").Attribute("value").Value);
-            owm.Clouds.Name = e.Element("clouds").Attribute("name").Value;
-
+            owm.Clouds = this.getClouds(e.Element("clouds"));
+            // visibility
             owm.Visibility = double.Parse(e.Element("visibility").Attribute("value").Value);
+            // precipitation
             owm.Precipitation = double.Parse(e.Element("precipitation").Attribute("value").Value);
-
             // Weather
-            owm.Weather.Id = int.Parse(e.Element("weather").Attribute("number").Value);
-            owm.Weather.Icon = e.Element("weather").Attribute("icon").Value;
-            owm.Weather.Description = e.Element("Weather").Attribute("value").Value;
-
+            owm.Weather = this.getWeather(e.Element("weather"));
             // last update
             owm.LastUpdate = DateTime.Parse(e.Element("lastupdate").Attribute("value").Value);
+
             return owm;
         }
         #endregion
