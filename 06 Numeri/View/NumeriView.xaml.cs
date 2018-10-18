71efe1d702b040ce5fa1070e96538a2019165f94
Giulio Bosco <giuliobva@gmail.com>
2018-10-18 11:06:28 +0200

    fix 06 Numeri - create app

---
+++ 06 Numeri/View/NumeriView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _06_Numeri.View {
+    /// <summary>
+    /// Interaction logic for Numero.xaml
+    /// </summary>
+    public partial class Numero : UserControl {
+        public Numero() {
+            InitializeComponent();
+        }
+    }
+}
