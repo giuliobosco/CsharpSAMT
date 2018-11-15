190d05a8686dfc5264a52121c3c6afda5cb5ef01
Giulio Bosco <giuliobva@gmail.com>
2018-11-15 11:22:58 +0100

    add 08 Coffee, Country

---
+++ 08 CoffeeShop.DA/Model/Country.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_CoffeeShop.DA.Model {
+    public enum Country {
+        Brasile,
+        Ecuador,
+        Etiopia,
+        Vietnam,
+        Colombia,
+        Peru,
+        Messico,
+        Honduras,
+        India,
+        Indonesia,
+        Guatemala
+    }
+}
