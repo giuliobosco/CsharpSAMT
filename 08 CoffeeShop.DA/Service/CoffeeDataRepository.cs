0e8ed650e1003e84732c0fa8bfc7d7f040d1ecad
giuliobosco <giuliobva@gmail.com>
2018-11-21 16:20:43 +0100

    add 08.DA CoffeeDataRepository

---
+++ 08 CoffeeShop.DA/Service/CoffeeDataRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _08_CoffeeShop.DA.Model;
+
+namespace _08_CoffeeShop.DA.Service {
+    public class CoffeeDataRepository : IDataRepository<Coffee> {
+        private DataContext ctx;
+
+        public CoffeeDataRepository() {
+            ctx = new DataContext();
+        }
+
+        public Coffee Get(int id) {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Coffee> Get() {
+            return ctx.Coffees;
+        }
+
+        public void Delete(Coffee entity) {
+            throw new NotImplementedException();
+        }
+
+        public Coffee Insert(Coffee entity) {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Coffee entity) {
+            throw new NotImplementedException();
+        }
+    }
+}
