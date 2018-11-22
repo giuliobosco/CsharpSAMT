af9d3d0852866c873b858df1a7b719fb30c86fdb
giuliobosco <giuliobva@gmail.com>
2018-11-22 10:50:04 +0100

    add 08 CoffeeList (View & ViewModel)

---
+++ 08 CoffeeShop/ViewModel/CoffeeListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVM;
+
+namespace _08_CoffeeShop.ViewModel {
+    public class CoffeeListViewModel : BindableBase {
+    }
+}
