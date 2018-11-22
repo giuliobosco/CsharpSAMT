8c730fc0a00a0e160ea6c8db7f0d5b3fa85ae500
giuliobosco <giuliobva@gmail.com>
2018-11-22 11:32:20 +0100

    add 08 MainViewModel

---
+++ 08 CoffeeShop/ViewModel/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVM;
+
+namespace _08_CoffeeShop.ViewModel {
+    public class MainViewModel: BindableBase {
+        #region =================== costants ===================
+        #endregion
+
+        #region =================== static Members =============
+        #endregion
+
+        #region =================== properties & members =======
+
+        private CoffeeListViewModel clvm;
+
+        private BindableBase currentViewModel;
+
+        public BindableBase CurrentViewModel {
+            get { return currentViewModel; }
+            set { SetProperty(ref currentViewModel, value); }
+        }
+
+        #endregion
+
+        #region =================== constructors ===============
+
+        public MainViewModel() {
+            clvm = new CoffeeListViewModel();
+        }
+
+        #endregion
+
+        #region =================== help methods ===============
+
+        #endregion
+
+        #region =================== general methods ============
+
+        #endregion
+    }
+}
