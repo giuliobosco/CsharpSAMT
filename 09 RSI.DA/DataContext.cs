6c30d4a33b18685098d260a3464cdc8b2269573f
giuliobosco <giuliobva@gmail.com>
2018-11-29 15:46:18 +0100

    add 09.DA DataContext

---
+++ 09 RSI.DA/DataContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _09_RSI.DA.Model;
+
+namespace _09_RSI.DA {
+    public class DataContext {
+        #region =================== costants ===================
+        #endregion
+
+        #region =================== static Members =============
+        #endregion
+
+        #region =================== properties & members =======
+
+        public List<Film> Films { get; private set; }
+
+        #endregion
+
+        #region =================== constructors ===============
+
+        public DataContext() {
+            Films = new List<Film>();
+        }
+
+        #endregion
+
+        #region =================== help methods ===============
+        #endregion
+
+        #region =================== general methods ============
+        #endregion
+    }
+}
