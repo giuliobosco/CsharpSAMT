46f62ea596fda890500734e70bba6c353d1552f5
giuliobosco <giuliobva@gmail.com>
2018-12-06 10:54:23 +0100

    add 09.DA FilmListViewModel

---
+++ 09 RSI/ViewModel/FilmListViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_RSI.ViewModel {
+    public class FilmListViewModel  {
+        #region =================== costants ===================
+        #endregion
+
+        #region =================== static Members =============
+        #endregion
+
+        #region =================== properties & members =======
+        #endregion
+
+        #region =================== constructors ===============
+        #endregion
+
+        #region =================== help methods ===============
+        #endregion
+
+        #region =================== general methods ============
+        #endregion
+    }
+}
