ba2c34fddd72bbf3ee5fdc84957187c376de8761
giuliobosco <giuliobva@gmail.com>
2018-12-06 11:04:12 +0100

    fix 09 FilmListViewModel - ObservableCollection

--- 09 RSI/ViewModel/FilmListViewModel.cs
+++ 09 RSI/ViewModel/FilmListViewModel.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MVVM;
+using _09_RSI.DA.Model;
+using _09_RSI.DA.Service;
 
 namespace _09_RSI.ViewModel {
-    public class FilmListViewModel  {
+    public class FilmListViewModel : BindableBase {
         #region =================== costants ===================
         #endregion
 
@@ -13,9 +17,18 @@ namespace _09_RSI.ViewModel {
         #endregion
 
         #region =================== properties & members =======
+
+        public ObservableCollection<Film> Films { get; set; }
+
         #endregion
 
         #region =================== constructors ===============
+
+        public FilmListViewModel() {
+            FilmDataRepository filmDataRepository = new FilmDataRepository();
+            Films = new ObservableCollection<Film>(filmDataRepository.Get());
+        }
+
         #endregion
 
         #region =================== help methods ===============
