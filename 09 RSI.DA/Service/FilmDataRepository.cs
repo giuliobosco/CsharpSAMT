230ef22fb13def8c6dc5447e9891f21b384aece5
giuliobosco <giuliobva@gmail.com>
2018-12-06 10:28:18 +0100

    fix 09.DA Service - namespace

--- 09 RSI.DA/Service/FilmDataRepository.cs
+++ 09 RSI.DA/Service/FilmDataRepository.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using _08_CoffeeShop.DA.Service;
 using _09_RSI.DA.Model;
 
 namespace _09_RSI.DA.Service {
--- 09 RSI.DA/Service/IDataRepository.cs
+++ 09 RSI.DA/Service/IDataRepository.cs
@@ -1,11 +1,11 @@
-﻿using _08_CoffeeShop.DA.Model;
+﻿using _09_RSI.DA.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _08_CoffeeShop.DA.Service {
+namespace _09_RSI.DA.Service {
     public interface IDataRepository<T> {
         T Get(int id);
 
