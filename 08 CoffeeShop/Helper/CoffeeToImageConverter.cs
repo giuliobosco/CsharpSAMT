57179c708155ef5a545e210254d77e61417fa51d
giuliobosco <giuliobva@gmail.com>
2018-12-19 15:48:09 +0100

    add 08 CoffeToImageConverter

---
+++ 08 CoffeeShop/Helper/CoffeeToImageConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace _08_CoffeeShop.Helper {
+    public class CoffeeToImageConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            try {
+                int id = (int) value;
+
+                string imgPath = "/Images/" + id + ".jpg";
+
+                BitmapImage img = new BitmapImage();
+                img.BeginInit();
+                img.UriSource = new Uri(imgPath, UriKind.RelativeOrAbsolute);
+                img.EndInit();
+
+                return img;
+
+            }
+            catch (FormatException fe) {
+                throw fe;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
