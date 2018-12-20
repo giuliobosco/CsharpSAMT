ce5f801aeda7f7fadb4a66c8bf8134e8ad7c8c44
giuliobosco <giuliobva@gmail.com>
2018-12-20 13:13:57 +0100

    fix 101 mercatino index

---
+++ 101 Mercatino/Controllers/MercatinoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _101_Mercatino.Models;
+
+namespace _101_Mercatino.Controllers
+{
+    public class MercatinoController : Controller
+    {
+        DataContext ctx = new DataContext();
+
+        // GET: Mercatino
+        public ActionResult Index() {
+            var dati = ctx.Mercatini.OrderBy(m => m.Luogo);
+            return View(dati);
+        }
+
+    }
+}
