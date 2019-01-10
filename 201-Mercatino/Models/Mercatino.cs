a984caa604fb9723e475c9f01670e7f5564e9ada
giuliobosco <giuliobva@gmail.com>
2019-01-10 11:02:42 +0100

    fix 201 Mercatino - table name

--- 201-Mercatino/Models/Mercatino.cs
+++ 201-Mercatino/Models/Mercatino.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace _101_Mercatino.Models {
+    [Table("Mercatini")]
     public class Mercatino {
         public int Id { get; set; }
         public string Luogo { get; set; }
