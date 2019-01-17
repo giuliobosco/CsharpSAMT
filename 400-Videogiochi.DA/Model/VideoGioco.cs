5a81fd3abe0d4b87fbd9d1113da624c206a23480
giuliobosco <giuliobva@gmail.com>
2019-01-17 10:59:00 +0100

    add 400.DA Model.VideoGioco

---
+++ 400-Videogiochi.DA/Model/VideoGioco.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _400_Videogiochi.DA.Model {
+    [Table("Videogiochi")]
+    public class VideoGioco {
+        [Key]
+        public int Id { get; set; }
+
+        public double Costo { get; set; }
+
+        public DateTime Data { get; set; }
+
+        public string Nome { get; set; }
+
+        public int Record { get; set; }
+    }
+}
