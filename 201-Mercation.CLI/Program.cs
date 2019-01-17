f9da80936600120228f45041f6bee553a52e036a
giuliobosco <giuliobva@gmail.com>
2019-01-17 10:36:54 +0100

    fix 201.CLI - Program execute query

--- 201-Mercation.CLI/Program.cs
+++ 201-Mercation.CLI/Program.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using _201_Mercatino;
 
-namespace _201_Mercation.CLI {
+namespace _201_Mercatino.CLI {
     class Program {
         static void Main(string [] args) {
+            MercatinoContext ctx = new MercatinoContext();
+
+            var dati = from n in ctx.Nazioni
+                orderby n.Nome
+                select n;
+
+            foreach (var item in dati) {
+                Console.WriteLine(item.Nome);
+            }
+
+            Console.ReadLine();
         }
     }
 }
