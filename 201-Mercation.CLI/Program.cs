d34ad26597a9d7a5bdb0b06470781956ccd620d3
giuliobosco <giuliobva@gmail.com>
2019-01-17 10:16:40 +0100

    add 201.CLI

---
+++ 201-Mercation.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _201_Mercation.CLI {
+    class Program {
+        static void Main(string [] args) {
+        }
+    }
+}
