5882289385704912f44ef30b50336ae4fe9b997f
giuliobosco <giuliobva@gmail.com>
2019-01-24 10:20:56 +0100

    fix 400.DA Tipo - table name

--- 04-VideoGioco/400-Videogiochi.DA/Model/Tipo.cs
+++ 04-VideoGioco/400-Videogiochi.DA/Model/Tipo.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _400_Videogiochi.DA.Model {
+    [Table("Tipi")]
     public class Tipo {
+        [Key]
         public int Id { get; set; }
 
         public string Nome { get; set; }
