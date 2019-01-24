6bc5d7c9080d1a1f5546f15dca18f11345025cfd
giuliobosco <giuliobva@gmail.com>
2019-01-24 10:20:34 +0100

    add 400.DA Record

---
+++ 04-VideoGioco/400-Videogiochi.DA/Model/Record.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _400_Videogiochi.DA.Model {
+    [Table("Records")]
+    public class Record {
+    }
+}
