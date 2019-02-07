e6b03c5df89fe38970cfdc04fc8f1646bf98a607
Giulio Bosco <giuliobva@gmail.com>
2019-02-07 10:27:25 +0100

    add GB03 01.DA Tesi

---
+++ GiulioBosco-03/01-Laurea.DA/Model/Tesi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Laurea.DA.Model {
+    [Table("Tesi")]
+    public class Tesi {
+        [Key]
+        public int Id;
+        public string Titolo;
+        public List<Studente> Studenti;
+        public Formatore Relatore;
+        public Formatore Perito;
+        public Tipo Tipo;
+        public Area Area;
+    }
+}
