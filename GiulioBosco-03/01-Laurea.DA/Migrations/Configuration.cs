6bbd9ddb64f3bce673e7ce3a9a255ebf72a505fb
Giulio Bosco <giuliobva@gmail.com>
2019-02-07 10:43:55 +0100

    fix GB03 03.DA Enable-Migrations

---
+++ GiulioBosco-03/01-Laurea.DA/Migrations/Configuration.cs
@@ -0,0 +1,33 @@
+using _01_Laurea.DA.Model;
+
+namespace _01_Laurea.DA.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<_01_Laurea.DA.LaureeModel>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(_01_Laurea.DA.LaureeModel context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
