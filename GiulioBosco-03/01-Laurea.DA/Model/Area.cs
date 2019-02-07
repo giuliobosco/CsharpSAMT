5e9badd437fd5b09680970720aea6c9bc2929715
Giulio Bosco <giuliobva@gmail.com>
2019-02-07 10:22:11 +0100

    add GB03 01.DA Area

---
+++ GiulioBosco-03/01-Laurea.DA/Model/Area.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Laurea.DA.Model {
+    [Table("Aree")]
+    class Area {
+        [Key]
+        public int Id;
+        public string Nome;
+    }
+}
