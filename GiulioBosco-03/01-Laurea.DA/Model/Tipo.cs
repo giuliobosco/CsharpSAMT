90b01ce29fff0d10fa056d2c8cb355a73bac25ce
Giulio Bosco <giuliobva@gmail.com>
2019-02-07 10:26:11 +0100

    add GB03 01.DA Tipo

---
+++ GiulioBosco-03/01-Laurea.DA/Model/Tipo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Laurea.DA.Model {
+    public enum Tipo {
+        ricerca,
+        rassegna,
+        progetto
+    }
+}
