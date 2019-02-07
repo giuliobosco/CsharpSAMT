dbb9db838bc548481bc60caabfac583b14153c83
Giulio Bosco <giuliobva@gmail.com>
2019-02-07 10:22:17 +0100

    add GB03 01.DA Formatore

---
+++ GiulioBosco-03/01-Laurea.DA/Model/Formatore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Laurea.DA.Model {
+    [Table("Formatori")]
+    class Formatore {
+        [Key]
+        public int Id;
+        public string Cognome;
+        public string Ente;
+        public bool Interno;
+    }
+}
