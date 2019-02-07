b3c8387666c02ce7baa81ce57750e08da865f0c8
Giulio Bosco <giuliobva@gmail.com>
2019-02-07 10:22:05 +0100

    add GB03 01.DA Studente

---
+++ GiulioBosco-03/01-Laurea.DA/Model/Studente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Laurea.DA.Model {
+    [Table("Studenti")]
+    class Studente {
+        [Key]
+        public int Id;
+        public string Nome;
+        public string Cognome;
+    }
+}
