e628a148c1e413ff1f8c6ab7c778cd8df3a0df0f
Giulio Bosco <giuliobva@gmail.com>
2019-02-14 10:45:28 +0100

    add gb03.WEB

---
+++ GiulioBosco-03/03-Laurea.WEB/App_Start/FilterConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace _03_Laurea.WEB {
+    public class FilterConfig {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ GiulioBosco-03/03-Laurea.WEB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_03_Laurea.WEB.MvcApplication" Language="C#" %>
