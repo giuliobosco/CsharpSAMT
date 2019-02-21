2b6f8ea1088289c44502a44f4e00d8f79113a4de
Giulio Bosco <giuliobva@gmail.com>
2019-02-21 10:21:17 +0100

    add GB03.DA IDataRepository

---
+++ GiulioBosco-03/03-Laurea.DA/Services/IDataRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Laurea.DA.Services {
+    interface IDataRepository<T> {
+        T Get(int index);
+
+        IEnumerable<T> Get();
+
+        void Insert(T element);
+
+        void Delete(T element);
+
+        void Update(T element);
+
+    }
+}
