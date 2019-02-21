d50d74dbcdfea2b042826164f7f7e79599c7bfdd
Giulio Bosco <giuliobva@gmail.com>
2019-02-21 11:33:42 +0100

    fix 05.WEB Startup

--- 05-TortaDellaNonna/Torta.WEB/Startup.cs
+++ 05-TortaDellaNonna/Torta.WEB/Startup.cs
@@ -6,23 +6,33 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Torta.DA.Services;
 
 namespace Torta.WEB {
     public class Startup {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
+            // registriamo i servizi del framework
+            services.AddMvc();
+            // registriamo i nostri servizi
+            services.AddTransient<ITortaRepository, MockTortaRepository>(); // ogni volta uno nuovo
+            //services.AddScoped //  uno per  ogni pagina
+            //services.AddSingleton // uno per tutta la app
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
-            if (env.IsDevelopment()) {
-                app.UseDeveloperExceptionPage();
-            }
+            //if (env.IsDevelopment()) {
+            app.UseDeveloperExceptionPage();
+            //}
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
 
-            app.Run(async (context) => {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            //app.Run(async (context) => {
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
     }
 }
