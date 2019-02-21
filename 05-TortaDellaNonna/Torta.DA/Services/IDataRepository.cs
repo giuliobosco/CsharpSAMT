f371e9d016c6bf236c8f1519338e17f1460b76bb
Giulio Bosco <giuliobva@gmail.com>
2019-02-21 10:44:28 +0100

    add 05.DA IDataRepositroy

---
+++ 05-TortaDellaNonna/Torta.DA/Services/IDataRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Torta.DA.Services {
+    public interface IDataRepository<T> {
+        T Get(int id);
+
+        IEnumerable<T> Get();
+
+        void Insert(T element);
+
+        void Delete(T element);
+
+        void Update(T element);
+    }
+}
