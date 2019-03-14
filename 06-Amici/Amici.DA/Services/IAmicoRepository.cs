d087f620f24c0585a352f23314ef58ad95cf73bd
Giulio Bosco <giuliobva@gmail.com>
2019-03-14 10:27:48 +0100

    add 06.DA IAmicoRepository.cs

---
+++ 06-Amici/Amici.DA/Services/IAmicoRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Amici.DA.Models;
+
+namespace Amici.DA.Services {
+    public interface IAmicoRepository : IDataRepository<Amico> {
+
+    }
+}
