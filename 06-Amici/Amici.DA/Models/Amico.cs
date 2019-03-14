37ebbc722b924078427c806d7d8a8b99f35fa846
Giulio Bosco <giuliobva@gmail.com>
2019-03-14 10:26:25 +0100

    add 06.DA Amico.cs

---
+++ 06-Amici/Amici.DA/Models/Amico.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Amici.DA.Models {
+    public class Amico {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Cognome { get; set; }
+
+        public string Luogo { get; set; }
+
+        public string Foto { get; set; }
+    }
+}
