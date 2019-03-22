13ae07cd52213e8057536cab150b6996ead0d2d7
Giulio Bosco <giuliobva@gmail.com>
2019-03-22 20:05:24 +0100

    add 05.DA prj

---
+++ 05-TortaDellaNonna/05-TortaDellaNonna.DA/Services/IDataRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace _05_TortaDellaNonna.DA.Services {
+    public interface IDataRepository<T> {
+        T Get(int id);
+
+        IEnumerable<T> Get();
+
+        T Insert(T entity);
+
+        void Delete(T entity);
+
+        void Update(T entity);
+    }
+}
