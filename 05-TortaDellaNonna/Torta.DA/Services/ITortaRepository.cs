39b9bb72f9a8bd69630043bc0085a20111c4e17c
Giulio Bosco <giuliobva@gmail.com>
2019-03-22 20:00:55 +0100

    rm 05*

--- 05-TortaDellaNonna/Torta.DA/Services/ITortaRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Torta.DA.Services {
-    public interface ITortaRepository : IDataRepository<Models.Torta> {
-    }
-}
--- 05-TortaDellaNonna/Torta5.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Torta5.Web.Models {
-    public class ErrorViewModel {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
