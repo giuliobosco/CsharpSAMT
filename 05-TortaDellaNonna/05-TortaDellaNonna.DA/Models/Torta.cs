1ff5d39c1b78a8669ca99bfa0eafd2bcef18e24b
Giulio Bosco <giuliobva@gmail.com>
2019-03-22 20:09:07 +0100

    fix 05.DA Torta

---
+++ 05-TortaDellaNonna/05-TortaDellaNonna.DA/Models/Torta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace _05_TortaDellaNonna.DA.Models {
+    public class Torta {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descrizione { get; set; }
+
+        public string Osservazioni { get; set; }
+
+        public bool TortaDelMese { get; set; }
+
+        public decimal Prezzo { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string ImageThumbnailUrl { get; set; }
+
+    }
+}
