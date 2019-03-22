f7b777c6b57e4a6bdc05ed48d9656391688fd28a
Giulio Bosco <giuliobva@gmail.com>
2019-03-22 20:40:52 +0100

    fix 05.CLI Program - UseMockRepository()

--- 05-TortaDellaNonna/05-TortaDellaNonna.CLI/Program.cs
+++ 05-TortaDellaNonna/05-TortaDellaNonna.CLI/Program.cs
@@ -4,7 +4,14 @@ using _05_TortaDellaNonna.DA.Services;
 namespace _05_TortaDellaNonna.CLI {
     class Program {
         static void Main(string[] args) {
+            UseMockRepository();
+        }
 
+        private static void UseMockRepository() {
+            foreach (var item in new MockTortaRepository().Get()) {
+                Console.Write("Nome: " + item.Nome);
+                Console.WriteLine("Prezzo: " + item.Prezzo + "\n");
+            }
         }
     }
 }
