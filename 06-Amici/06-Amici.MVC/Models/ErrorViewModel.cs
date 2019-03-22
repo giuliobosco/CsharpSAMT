bdf9bd26ff3ecf4f55e721b7a5e18711bf4f9e9c
Giulio Bosco <giuliobva@gmail.com>
2019-03-22 09:42:01 +0000

    add 06.MVC

---
+++ 06-Amici/06-Amici.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace _06_Amici.MVC.Models {
+    public class ErrorViewModel {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
