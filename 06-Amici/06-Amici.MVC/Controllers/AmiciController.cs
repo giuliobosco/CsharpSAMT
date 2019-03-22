00b3d0b0ad6fe2c7a7c511ed899b835045a7fd69
Giulio Bosco <giuliobva@gmail.com>
2019-03-22 17:30:46 +0000

    fix 06.MVC AmiciController - Create()

--- 06-Amici/06-Amici.MVC/Controllers/AmiciController.cs
+++ 06-Amici/06-Amici.MVC/Controllers/AmiciController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using _06_Amici.DA.Services;
+using _06_Amici.DA.Model;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -19,6 +20,23 @@ namespace _06_Amici.MVC.Controllers {
         // GET: /<controller>/
         public IActionResult Index() {
             return View(ctx.Get());
+        }
+
+        // GET: Amici/Create
+        public ActionResult Create() {
+            return View();
+        }
+
+        // POST: Amici/Create
+        [HttpPost]
+        public ActionResult Create(Amico entity) {
+            try {
+                Console.WriteLine(entity.Nome);
+                ctx.Insert(entity);
+                return RedirectToAction("Index");
+            } catch {
+                return View();
+            }
         }
     }
 }
