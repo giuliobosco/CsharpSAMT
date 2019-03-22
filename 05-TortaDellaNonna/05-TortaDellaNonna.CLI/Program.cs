69d2b6cff81acf5b7bb63d76b8b931cf4f631317
Giulio Bosco <giuliobva@gmail.com>
2019-03-22 20:39:29 +0100

    add 05.CLI prj

---
+++ 05-TortaDellaNonna/05-TortaDellaNonna.CLI/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using _05_TortaDellaNonna.DA.Models;
+using _05_TortaDellaNonna.DA.Services;
+namespace _05_TortaDellaNonna.CLI {
+    class Program {
+        static void Main(string[] args) {
+
+        }
+    }
+}
