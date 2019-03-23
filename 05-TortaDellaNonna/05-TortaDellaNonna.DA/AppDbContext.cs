f3cb22b80600bec4a3c24aab01f5ca088c934ef3
Giulio Bosco <giuliobva@gmail.com>
2019-03-23 22:44:19 +0100

    add 05.DA AppDbContext

---
+++ 05-TortaDellaNonna/05-TortaDellaNonna.DA/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using _05_TortaDellaNonna.DA.Models;
+
+namespace _05_TortaDellaNonna.DA {
+    public class AppDbContext : DbContext {
+
+        public DbSet<Torta> Torte { get; set; }
+
+        public AppDbContext() {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\mssqllocaldb;" +
+                "DataBase=TortaNonna;" + 
+                "AttachDbFilename=/Users/Shared/TortaNonna.mdf;" +
+                "Trusted_Connection=true"
+                );
+        }
+    }
+}
