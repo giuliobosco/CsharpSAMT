ef52b79a10a7165b8489c654a21fb0658c3cb301
Giulio Bosco <giuliobva@gmail.com>
2019-03-28 10:18:26 +0100

    add 06.DA Aiuto

---
+++ 06-Amici/06-Amici.DA/Models/Aiuto.cs
@@ -0,0 +1,14 @@
+namespace _06_Amici.DA.Models {
+	public class Aiuto {
+		public int Id { get; set; }
+
+		public string Messaggio { get; set; }
+
+		public string Email { get; set; }
+
+		public string Nome { get; set; }
+
+		public bool Contattami { get; set; }
+		
+	}
+}
