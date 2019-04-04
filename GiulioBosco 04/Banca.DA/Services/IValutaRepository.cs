915216a6d02075612db9a75ae4693eecc6787d3f
Giulio Bosco <giuliobva@gmail.com>
2019-04-04 10:23:12 +0200

    add GB04.DA IValutaRepository

---
+++ GiulioBosco 04/Banca.DA/Services/IValutaRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Banca.DA.Models;
+
+namespace Banca.DA.Services {
+    public interface IValutaRepository : IDataRepository<Valuta> {
+
+    }
+}
