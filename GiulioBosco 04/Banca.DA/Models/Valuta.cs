91551fe1148f226f4651420b032c1a55a0493fef
Giulio Bosco <giuliobva@gmail.com>
2019-04-04 10:21:55 +0200

    add GB04.DA Valuta

---
+++ GiulioBosco 04/Banca.DA/Models/Valuta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banca.DA.Models {
+    public class Valuta {
+        public int Id { get; set; }
+
+        public string Codice { get; set; }
+
+        public string  denominazione { get; set; }
+
+        public double entrata { get; set; }
+
+        public double cambio { get; set; }
+    }
+}
