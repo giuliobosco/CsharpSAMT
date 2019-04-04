8c5f72500dadf60ba3f73114b7b979035473cf10
Giulio Bosco <giuliobva@gmail.com>
2019-04-04 10:30:37 +0200

    add GB04.DA AppDbContext

---
+++ GiulioBosco 04/Banca.DA/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Banca.DA.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Banca.DA {
+    public class AppDbContext : DbContext {
+        public DbSet<Valuta> Valute { get; set; }
+
+        public AppDbContext() : base() {
+
+        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;" +
+                                        "Database=Amici.3AA;" +
+                                        "AttachDbFilename=c:\\tmp\\amici.3AA.mdf;" +
+                                        "Trusted_Connection=true");
+        }
+
+    }
+}
