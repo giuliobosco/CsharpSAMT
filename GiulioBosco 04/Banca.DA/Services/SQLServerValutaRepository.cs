c591240dc412787856cecd1196be6825a4be8211
Giulio Bosco <giuliobva@gmail.com>
2019-04-04 10:33:53 +0200

    add GB04.DA SQLServerValutaRepository

---
+++ GiulioBosco 04/Banca.DA/Services/SQLServerValutaRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Banca.DA.Models;
+
+namespace Banca.DA.Services {
+    class SQLServerValutaRepository : IValutaRepository {
+
+        private readonly AppDbContext ctx;
+
+        public SQLServerValutaRepository(AppDbContext context) {
+            ctx = context;
+        }
+
+        public Valuta Get(int id) {
+            return ctx.Valute.FirstOrDefault(a => a.Id == id);
+        }
+
+        public IEnumerable<Valuta> Get() {
+            return ctx.Valute;
+        }
+
+        public void Delete(Valuta entity) {
+            ctx.Entry(entity).State = Microsoft.EntityFrameworkCore.EntityState.Deleted;
+            ctx.SaveChanges();
+        }
+
+        public Valuta Insert(Valuta entity) {
+            ctx.Entry(entity).State = Microsoft.EntityFrameworkCore.EntityState.Added;
+            ctx.SaveChanges();
+            return entity;
+        }
+
+        public void Update(Valuta entity) {
+            ctx.Entry(entity).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            ctx.SaveChanges();
+        }
+    }
+}
