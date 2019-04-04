a2bc9556d4c4d8ecbdbdf8d9f10ea3411e5981fc
Giulio Bosco <giuliobva@gmail.com>
2019-04-04 10:22:07 +0200

    add GB04.DA IDataRepository

---
+++ GiulioBosco 04/Banca.DA/Services/IDataRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banca.DA.Services {
+    public interface IDataRepository<T> {
+        T Get(int id);
+        IEnumerable<T> Get();
+        void Delete(T entity);
+        T Insert(T entity);
+        void Update(T entity);
+    }
+}
