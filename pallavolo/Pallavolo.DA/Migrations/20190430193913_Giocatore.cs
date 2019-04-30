2ff6f15b3ede2d44fe2c43fe3b5878ed14f4384f
Giulio Bosco <giuliobva@gmail.com>
2019-04-30 21:41:49 +0200

    fix Pallavolo.DA Migrations
    dotnet ef migrations add Giocatore

---
+++ pallavolo/Pallavolo.DA/Migrations/20190430193913_Giocatore.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pallavolo.DA.Migrations
+{
+    public partial class Giocatore : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Giocatori",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Added = table.Column<DateTime>(nullable: false),
+                    Deleted = table.Column<DateTime>(nullable: false),
+                    Modified = table.Column<DateTime>(nullable: false),
+                    Nome = table.Column<string>(nullable: true),
+                    Cognome = table.Column<string>(nullable: true),
+                    Nascita = table.Column<DateTime>(nullable: false),
+                    numero = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Giocatori", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Giocatori");
+        }
+    }
+}
