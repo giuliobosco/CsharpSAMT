5978cc588e405b5a07c955fe91486e41ec303f36
Giulio Bosco <giuliobva@gmail.com>
2019-04-30 22:55:40 +0200

    fix Pallavolo.DA add Migration (FixGiocatore)

---
+++ pallavolo/Pallavolo.DA/Migrations/20190430205326_FixGiocatore.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pallavolo.DA.Migrations
+{
+    public partial class FixGiocatore : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "numero",
+                table: "Giocatori",
+                newName: "Numero");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Numero",
+                table: "Giocatori",
+                newName: "numero");
+        }
+    }
+}
